6ccd1fe6636a74d176e199b9c16a2998c5db07fc
firas hamila <firas.hm.1996@gmail.com>
2025-02-18 09:11:43 +0100

    a

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const User = require("../models/userModel");
+
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      message: "User created !!!",
+      data: { newUser },
+    });
+  } catch (error) {}
+};
